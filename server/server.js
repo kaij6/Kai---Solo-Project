fa76e241fc0f9476c4d677a2a2237433c2ac81b8
Kai Jiang <kaixinjiang66@gmail.com>
2024-07-30 04:46:18 -0700

    created

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+// Connect to MongoDB
+const mongoURI = 'mongodb+srv://kaixinjiang66:Fgh245245@cluster0.gc9zwz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+// Basic route
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
